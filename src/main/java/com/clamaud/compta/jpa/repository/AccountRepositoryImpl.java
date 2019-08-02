17ded3c2a6d08ba7d0e226fcf4fef17a2d363568
Cédric LAMAUD <c.lamaud@treeptik.fr>
2019-08-02 15:41:47 +0200

    Modif Date

--- src/main/java/com/clamaud/compta/jpa/repository/AccountRepositoryImpl.java
+++ src/main/java/com/clamaud/compta/jpa/repository/AccountRepositoryImpl.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TemporalType;
 import javax.persistence.TypedQuery;
 
 import com.clamaud.compta.jpa.account.Account;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 
 public class AccountRepositoryImpl {
 
@@ -26,6 +28,11 @@ public class AccountRepositoryImpl {
 		sb.append(" and (:subCategory is null or a.subCategory = :subCategory)");
 		sb.append(" and (:dateFrom is null or a.date >= :dateFrom)");
 		sb.append(" and (:dateTo is null or a.date <= :dateTo)");
+		
+		if (criteria.isExpensesOnly()) {
+			sb.append(" and (a.amount < 0)");
+		}
+		
 		sb.append(" order by a.date");
 		
                 
@@ -33,9 +40,9 @@ public class AccountRepositoryImpl {
 
 		query.setParameter("category", criteria.getCategory());
         query.setParameter("subCategory", criteria.getSubCategory());
-        query.setParameter("dateFrom", criteria.getDateFrom());
-        query.setParameter("dateTo", criteria.getDateTo());
-		
+        query.setParameter("dateFrom", criteria.getDateFrom(), TemporalType.DATE);
+        query.setParameter("dateTo", criteria.getDateTo(), TemporalType.DATE);
+        
 		return query.getResultList();
 	}
 
